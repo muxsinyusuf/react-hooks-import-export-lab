6cf9f4ff8cfd8518f145c651ae7a7186002fa8d0
muxsinyusuf <mucsin.yusuf@student.moringaschool.com>
2023-08-15 18:15:52 +0300

    Export and Import Mastered

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,7 @@
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
 import App from "../components/App";
+import About from "./About.test";
 
 test("it renders without crashing", () => {
   try {
